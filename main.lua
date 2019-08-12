2e06b8d0b22bef6b274bb051657bec0c6a1ac930
Gino Tria <gino.tr14@gmail.com>
2019-08-13 07:31:38 +0800

    added bird object

--- main.lua
+++ main.lua
@@ -15,6 +15,12 @@
 -- virtual resolution handling library
 push = require 'push'
 
+-- classic OOP class library
+Class = require 'class'
+
+-- bird class we've written
+require 'Bird'
+
 -- physical screen dimensions
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -37,6 +43,9 @@ local GROUND_SCROLL_SPEED = 60
 
 -- point at which we should loop our background back to X 0
 local BACKGROUND_LOOPING_POINT = 413
+
+-- our bird sprite
+local bird = Bird()
 
 function love.load()
     -- initialize our nearest-neighbor filter
@@ -87,6 +96,9 @@ function love.draw()
     -- draw the ground on top of the background, toward the bottom of the screen,
     -- at its negative looping point
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+    -- render our bird to the screen using its own render logic
+    bird:render()
 
     push:finish()
 end
